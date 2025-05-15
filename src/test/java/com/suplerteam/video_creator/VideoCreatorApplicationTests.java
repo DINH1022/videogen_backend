e0912710301208e3959e53232c4cc143466d5479
Dreamer-Guy <CoolGuyLearning35@gmail.com>
2025-05-15 17:23:47 +0700

    construct project

---
+++ src/test/java/com/suplerteam/video_creator/VideoCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suplerteam.video_creator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
