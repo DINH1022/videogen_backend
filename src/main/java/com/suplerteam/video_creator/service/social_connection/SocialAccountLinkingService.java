9778101736e91efb6a8b32df33ec54297d52be51
Dreamer-Guy <CoolGuyLearning35@gmail.com>
2025-07-04 20:53:51 +0700

    [IMPLEMENTATION] implement get account linking status
    [REFACTOR]change name _SocialConnectionService to _SocialAccountLinkingService

--- src/main/java/com/suplerteam/video_creator/service/social_connection/SocialAccountLinkingService.java
+++ src/main/java/com/suplerteam/video_creator/service/social_connection/SocialAccountLinkingService.java
@@ -1,6 +1,6 @@
 package com.suplerteam.video_creator.service.social_connection;
 
-public interface SocialConnectionService {
+public interface SocialAccountLinkingService {
     String getAuthURL(Long userId);
     String getRefreshTokenByAuthCode(String authorizationCode);
     Boolean connectToSocialAccount(Long userId,String authorizationCode);
