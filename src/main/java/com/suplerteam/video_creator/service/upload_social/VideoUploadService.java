19d8e6744f1fe6e6a660758edcaf6f93f6f61bf1
Dreamer-Guy <CoolGuyLearning35@gmail.com>
2025-07-04 11:04:31 +0700

    [CHANGE] change interface VideoUploadService- upload method return String instead of Boolean

--- src/main/java/com/suplerteam/video_creator/service/upload_social/VideoUploadService.java
+++ src/main/java/com/suplerteam/video_creator/service/upload_social/VideoUploadService.java
@@ -3,5 +3,5 @@ package com.suplerteam.video_creator.service.upload_social;
 import com.suplerteam.video_creator.request.social_video_upload.SocialVideoUploadRequest;
 
 public interface VideoUploadService {
-    Boolean upload(SocialVideoUploadRequest req);
+    String upload(SocialVideoUploadRequest req);
 }
